b8bf094564e89cc315b91470b88a756e66372620
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2022-05-21 16:01:18 +0800

    Added small menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  	
+  	$('#small-menu').click(function(e){
+        e.preventDefault();
+
+        console.log('smallmenu')
+        $('.small-nav').toggleClass('active');
+        $('.small-menu').toggleClass('active');
+    });
 });
